13712b62cc44af3cb7b192033fa5831ea9d8457c
kosinskyPL <kamil@Kamils-MacBook-Pro.local>
2017-03-21 00:55:01 +0100

    Java lab4 upd
    updated event Handler for Swing

--- Programming-in-Java/Lab4/FirstJFrame.java
+++ Programming-in-Java/Lab4/FirstJFrame.java
@@ -1,24 +1,26 @@
 import java.awt.*;
 import javax.swing.*;
 
-
 public class FirstJFrame
 {	
 	public static void main(String[] args)
 	{
-		EventQueue.invokeLater(() ->
+		Runnable eventHandler = new Runnable()
 		{
-			JFrame frame = new JFrame();
-			Toolkit kit = Toolkit.getDefaultToolkit();
-			Dimension screenSize = kit.getScreenSize();
-			Component comp = new FigureComponent(screenSize.width/4, screenSize.height/4);
-			//Container contentPane = getContentPane();
-	
-			frame.setBounds(screenSize.width/4, screenSize.height/4, screenSize.width/2, screenSize.height/2);
-			frame.setResizable(false);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.add(comp);
-			frame.setVisible(true);
-		});
+			public void run(){
+				JFrame frame = new JFrame();
+				Toolkit kit = Toolkit.getDefaultToolkit();
+				Dimension screenSize = kit.getScreenSize();
+				Component comp = new FigureComponent(screenSize.width/4, screenSize.height/4);
+		
+				frame.setBounds(screenSize.width/4, screenSize.height/4, screenSize.width/2, screenSize.height/2);
+				frame.setResizable(false);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.add(comp);
+				frame.setVisible(true);
+			}	
+		};
+
+		SwingUtilities.invokeLater(eventHandler);
 	}
 }
