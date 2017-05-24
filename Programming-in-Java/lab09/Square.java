0e1e185cdad68d5e5a845d41ab5577035c8ba98b
Paul Kania <pawel.kania@onet.pl>
2017-05-24 22:50:01 +0200

    Fix resume thread

--- Programming-in-Java/lab09/Square.java
+++ Programming-in-Java/lab09/Square.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 public class Square extends JPanel
 {
     public static final int DEFAULT_SIZE = 10;
+    private final JPanel parent;
     private final Position position;
     private final double speed;
     private final int border;
@@ -22,27 +23,11 @@ public class Square extends JPanel
 
     public Square(final JPanel parent, int size)
     {
+        this.parent = parent;
         flag = new AtomicBoolean();
         border = parent.getHeight();
         speed = new Random().nextDouble() * 8 + 0.5;
         position = new Position(new Random().nextInt(parent.getWidth()), 0);
-        falling = new Thread(() -> {
-            while(flag.get())
-            {
-                position.translate(0, speed);
-                if(position.getY() > border)
-                    position.setY(0);
-                    
-                setLocation(position.toPoint());
-                parent.repaint();
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException ex) {
-                    break;
-                }
-            }
-        });
 
         setSize(new Dimension(size, size));
         setLocation(position.toPoint());
@@ -55,6 +40,23 @@ public class Square extends JPanel
     public void start()
     {
         flag.set(true);
+        falling = new Thread(() -> {
+            while(flag.get())
+            {
+                position.translate(0, speed);
+                if(position.getY() > border)
+                    position.setY(0);
+                    
+                setLocation(position.toPoint());
+                parent.repaint();
+
+                try {
+                    Thread.sleep(16);
+                } catch (InterruptedException ex) {
+                    break;
+                }
+            }
+        });
         falling.start();
     }
 
